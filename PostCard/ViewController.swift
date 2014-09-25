0f281c75b9597c5f42668816ff7197dc2c6ade0a
youshengchang <changyousheng@yahoo.com>
2014-09-24 20:29:45 -0700

    first swift

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var messageLabel: UILabel!
+    
+    @IBOutlet var enterNameTextField: UITextField!
+    
+    
+    @IBOutlet var enterMessageTextField: UITextField!
+    
+    @IBOutlet var messageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +30,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func sendMessageButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        messageButton.setTitle("Message Sent", forState: UIControlState.Normal)
+        
+        
+        
+        
+    }
 }
 
